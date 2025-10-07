48b90db031c84dd2dd6eafcbd7c452747ae32b2f
a <bv>
2025-10-07 15:03:21 +0200

    7

--- device.cpp
+++ device.cpp
@@ -2,6 +2,36 @@
 
 Device::Device() {}
 
+void Device::home()
+{
+
+}
+
+void Device::moveTo(double x, double y, double z)
+{
+
+}
+
+void Device::setSpeed(double speed)
+{
+
+}
+
+void Device::reset()
+{
+
+}
+
+void Device::setAbsolutePositioning()
+{
+
+}
+
+void Device::setRelativePositioning()
+{
+
+}
+
 void Device::send(QString line) {}
 
 void Device::waitResponses() {}
